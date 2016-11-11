367ff30181ce7795a19ec1f2213b622b64f2eebb
Andre Izquierdo <andre-izq@hotmail.com>
2016-11-10 23:14:34 -0600

    Views de usuarios modificada

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,8 +3,6 @@ class Usuario < ApplicationRecord
   validates_presence_of :nombre
   validates_presence_of :contraseña
 
-
-
   has_and_belongs_to_many :junta
   has_and_belongs_to_many :proyectos
   has_and_belongs_to_many :actividads
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110025947) do
+ActiveRecord::Schema.define(version: 20161111003921) do
 
   create_table "actividads", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nombre"
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 20161110025947) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "permiso"
-    t.boolean  "estado"
+    t.integer  "estado"
   end
 
 end
