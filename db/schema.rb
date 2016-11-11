680c4a62ca145f6aaf9a995a22ed5b087f5d95dd
Andre Izquierdo <andre-izq@hotmail.com>
2016-11-11 13:12:29 -0600

    Pendejada arreglada

--- db/schema.rb
+++ db/schema.rb
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 20161111165847) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "permiso"
-    t.boolean  "estado"
+    t.integer  "estado"
   end
 
 end
