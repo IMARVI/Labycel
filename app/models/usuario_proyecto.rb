9717e34b9591982ccc1dee8559e0567eb7ad7fc1
isaiasmarvi@gmail.com <isaiasmarvi@gmail.com>
2016-11-14 17:44:18 -0600

    Relacion completa

--- app/models/usuario_proyecto.rb
+++ app/models/usuario_proyecto.rb
@@ -1,6 +1,5 @@
 class UsuarioProyecto < ApplicationRecord
-  validates_presence_of :usuario_id
-  validates_presence_of :proyecto_id
+
   belongs_to :usuario
   belongs_to :proyecto
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :usuario_junta
   resources :usuario_proyectos
   resources :actividads
   resources :proyectos
